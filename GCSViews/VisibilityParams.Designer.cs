3089080a6a101c3e14e46246e891ee8caee96d5e
Nikita Galkin <54286964+GAJIbl4@users.noreply.github.com>
2025-07-01 11:05:24 +0300

    saving progress

--- GCSViews/VisibilityParams.Designer.cs
+++ GCSViews/VisibilityParams.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.menuButtonsPage = new System.Windows.Forms.TabPage();
-            this.mapContextMenuPage = new System.Windows.Forms.TabPage();
             this.menuButtonsChecklist = new System.Windows.Forms.CheckedListBox();
+            this.mapContextMenuPage = new System.Windows.Forms.TabPage();
             this.mapContextMenuChecklist = new System.Windows.Forms.CheckedListBox();
             this.tabControl1.SuspendLayout();
             this.menuButtonsPage.SuspendLayout();
@@ -52,42 +52,46 @@
             // menuButtonsPage
             // 
             this.menuButtonsPage.Controls.Add(this.menuButtonsChecklist);
-            this.menuButtonsPage.Location = new System.Drawing.Point(8, 39);
+            this.menuButtonsPage.Location = new System.Drawing.Point(4, 22);
             this.menuButtonsPage.Name = "menuButtonsPage";
             this.menuButtonsPage.Padding = new System.Windows.Forms.Padding(3);
-            this.menuButtonsPage.Size = new System.Drawing.Size(1074, 582);
+            this.menuButtonsPage.Size = new System.Drawing.Size(1082, 603);
             this.menuButtonsPage.TabIndex = 0;
             this.menuButtonsPage.Text = "Menu buttons";
             this.menuButtonsPage.UseVisualStyleBackColor = true;
             // 
-            // mapContextMenuPage
-            // 
-            this.mapContextMenuPage.Controls.Add(this.mapContextMenuChecklist);
-            this.mapContextMenuPage.Location = new System.Drawing.Point(8, 39);
-            this.mapContextMenuPage.Name = "mapContextMenuPage";
-            this.mapContextMenuPage.Padding = new System.Windows.Forms.Padding(3);
-            this.mapContextMenuPage.Size = new System.Drawing.Size(1074, 582);
-            this.mapContextMenuPage.TabIndex = 1;
-            this.mapContextMenuPage.Text = "Map context menu";
-            this.mapContextMenuPage.UseVisualStyleBackColor = true;
-            // 
             // menuButtonsChecklist
             // 
+            this.menuButtonsChecklist.CheckOnClick = true;
             this.menuButtonsChecklist.Dock = System.Windows.Forms.DockStyle.Fill;
             this.menuButtonsChecklist.FormattingEnabled = true;
             this.menuButtonsChecklist.Location = new System.Drawing.Point(3, 3);
             this.menuButtonsChecklist.Name = "menuButtonsChecklist";
-            this.menuButtonsChecklist.Size = new System.Drawing.Size(1068, 576);
+            this.menuButtonsChecklist.Size = new System.Drawing.Size(1076, 597);
             this.menuButtonsChecklist.TabIndex = 0;
+            this.menuButtonsChecklist.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.menuButtonsChecklist_ItemCheck);
+            // 
+            // mapContextMenuPage
+            // 
+            this.mapContextMenuPage.Controls.Add(this.mapContextMenuChecklist);
+            this.mapContextMenuPage.Location = new System.Drawing.Point(4, 22);
+            this.mapContextMenuPage.Name = "mapContextMenuPage";
+            this.mapContextMenuPage.Padding = new System.Windows.Forms.Padding(3);
+            this.mapContextMenuPage.Size = new System.Drawing.Size(1082, 603);
+            this.mapContextMenuPage.TabIndex = 1;
+            this.mapContextMenuPage.Text = "Map context menu";
+            this.mapContextMenuPage.UseVisualStyleBackColor = true;
             // 
             // mapContextMenuChecklist
             // 
+            this.mapContextMenuChecklist.CheckOnClick = true;
             this.mapContextMenuChecklist.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mapContextMenuChecklist.FormattingEnabled = true;
             this.mapContextMenuChecklist.Location = new System.Drawing.Point(3, 3);
             this.mapContextMenuChecklist.Name = "mapContextMenuChecklist";
-            this.mapContextMenuChecklist.Size = new System.Drawing.Size(1068, 576);
+            this.mapContextMenuChecklist.Size = new System.Drawing.Size(1076, 597);
             this.mapContextMenuChecklist.TabIndex = 0;
+            this.mapContextMenuChecklist.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.mapContextMenuChecklist_ItemCheck);
             // 
             // VisibilityParams
             // 
--- GCSViews/VisibilityParams.cs
+++ GCSViews/VisibilityParams.cs
@@ -23,6 +23,7 @@ namespace MissionPlanner.GCSViews
             InitializeComponent();
             log.Info("Visibility params constructor called");
             this.makeButtonCheckboxes();
+            this.makeContextMapCheckboxes();
 
         }
 
@@ -45,6 +46,7 @@ namespace MissionPlanner.GCSViews
             try
             {
                 var mainForm = MainV2.instance;
+
                 var menuButtons = mainForm.MainMenu.Items.OfType<ToolStripButton>().ToList();
 
                 foreach (var button in menuButtons)
@@ -53,7 +55,8 @@ namespace MissionPlanner.GCSViews
                     {
                         continue; // Skip MenuConnect and ArduPilot
                     }
-                    this.menuButtonsChecklist.Items.Add(button.Text, button.Visible);
+                    var checkboxItem = new MyCheckBoxItem(button.Name, button.Text, button.Visible);
+                    this.menuButtonsChecklist.Items.Add(checkboxItem, checkboxItem.IsChecked);
                 }
             }
             catch (Exception ex)
@@ -61,6 +64,137 @@ namespace MissionPlanner.GCSViews
                 log.Error("Error in MakeButtonCheckboxes", ex);
                 MessageBox.Show($"Error in MakeButtonCheckboxes: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
+        }
+
+        public void makeContextMapCheckboxes()
+        {
+            try
+            {
+                var mainForm = MainV2.instance;
+                var contextMapButtons = mainForm.FlightData.contextMenuStripMap.Items.OfType<ToolStripMenuItem>().ToList();
+                var allowedList = new HashSet<string>(Settings.Instance.GetList("AllowedContextMenuMapItems"));
+
+                foreach (var button in contextMapButtons)
+                {
+                    var isChecked = true;
+                    if (allowedList.Contains(button.Name))
+                    {
+                        isChecked = false;
+                    }
+                    var checkboxItem = new MyCheckBoxItem(button.Name, button.Text, isChecked);
+                    this.mapContextMenuChecklist.Items.Add(checkboxItem, checkboxItem.IsChecked);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error in MakeContextMapCheckboxes", ex);
+                MessageBox.Show($"Error in MakeContextMapCheckboxes: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void menuButtonsChecklist_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            try
+            {
+                var mainForm = MainV2.instance;
+
+                var currentItem = menuButtonsChecklist.Items[e.Index] as MyCheckBoxItem;
+                if (currentItem != null)
+                {
+                    // Обновляем свойство IsChecked в зависимости от нового состояния
+                    currentItem.IsChecked = e.NewValue == CheckState.Checked;
+                }
+
+                foreach (MyCheckBoxItem item in menuButtonsChecklist.Items)
+                {
+                    try
+                    {
+                        var key = "MenuVisible_" + item.Name;
+
+                        // Check if the setting exists, if not, set default value (True)
+                        if (!Settings.Instance.ContainsKey(key))
+                        {
+                            log.Info($"Setting {key} not found. Adding default value: True");
+                            Settings.Instance[key] = true.ToString();
+                        }
+
+                        log.Info($"Current {key} is {Settings.Instance.GetBoolean(key)}");
+                        Settings.Instance[key] = item.IsChecked.ToString();
+                    }
+                    catch (Exception ex)
+                    {
+                        log.Error($"Error updating settings for item {item.Name}", ex);
+                        MessageBox.Show($"Error updating settings for item {item.Name}: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                Settings.Instance.Save();
+                mainForm.ApplySavedMenuVisibility();
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error in MenuButtonsChecklist_SelectedIndexChanged", ex);
+                MessageBox.Show($"Error in MenuButtonsChecklist_SelectedIndexChanged: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void updateSettings()
+        {
+
+        }
+
+        private HashSet<string> GetMapContextMenuChecklistValues()
+        {
+            var hashSet = new HashSet<string>();
+
+            foreach (MyCheckBoxItem item in mapContextMenuChecklist.Items)
+            {
+                if (item.IsChecked)
+                {
+                    hashSet.Add(item.Name);
+                }
+            }
+
+            return hashSet;
+        }
+
+        private void mapContextMenuChecklist_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            try
+            {
+                var mainForm = MainV2.instance;
+                var currentItem = mapContextMenuChecklist.Items[e.Index] as MyCheckBoxItem;
+                if (currentItem != null)
+                {
+                    currentItem.IsChecked = e.NewValue == CheckState.Checked;
+                }
+
+                var allowedList = GetMapContextMenuChecklistValues();
+                Settings.Instance.SetList("AllowedContextMenuMapItems", allowedList);
+                mainForm.ApplySavedMenuVisibility();
+            }
+            catch (Exception ex)
+            {
+                log.Error("Error in MapContextMenuChecklist_ItemCheck", ex);
+                MessageBox.Show($"Error in MapContextMenuChecklist_ItemCheck: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+
+    public class MyCheckBoxItem
+    {
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public bool IsChecked { get; set; }
+        public MyCheckBoxItem(string name, string text, bool isChecked)
+        {
+            Name = name;
+            Text = text;
+            IsChecked = isChecked;
+        }
+        public override string ToString()
+        {
+            return Text;
         }
     }
 }
